77be3480387aa7fc8f1f11849ca88f18b4ccd4b3
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2023-08-08 14:57:18 +0900

    Create Main.java

---
+++ 재귀와 정렬/기초10870. 피보나치 수 5/권준구/Main.java
@@ -0,0 +1,23 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		System.out.println(Fibonacci(N));
+	}
+
+	public static int Fibonacci(int N) {
+		if (N == 0)
+			return 0;
+		if (N <= 1)
+			return 1;
+		return Fibonacci(N - 2) + Fibonacci(N - 1);
+	}
+}
