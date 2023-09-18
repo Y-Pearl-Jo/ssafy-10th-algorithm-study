213bd335f709b970fe6def1cdbf04974a62d482d
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-09-18 14:15:34 +0900

    Update Main(Bottom-Up).java

--- DP/기초2156. 포도주 시식/이재신/Main(Bottom-Up).java
+++ DP/기초2156. 포도주 시식/이재신/Main(Bottom-Up).java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//Bottom-Up 13224 KB 104 ms
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int N;
