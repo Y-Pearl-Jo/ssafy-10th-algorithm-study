6e94af864ba8cfddadd7ca94e48fffc4618c9fe6
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-09-18 14:33:09 +0900

    Update Main.java

--- DP/기초2193. 이친수/이재신/Main.java
+++ DP/기초2193. 이친수/이재신/Main.java
@@ -1,18 +1,33 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+// Bottom-Up 11492 KB 76 ms
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 
-public class Main{
-    public static void main(String args[]) throws IOException{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int count = Integer.parseInt(br.readLine());
-            long dp[] = new long[count+1];
-            // d[n] = n자리 이친수.
-            dp[0] = 0;
+	static int N, count;
+	static int[] num;
+	static Long[] dp;
+
+    static void solution(){
+        // d[n] = n자리 이친수.
+        dp[0] = 0;
             dp[1] = 1;
             for (int i = 2; i <= count; i++){
                 dp[i] = dp[i-1] + dp[i-2];
             }
             System.out.println(dp[count]);
+    }
+
+    static void make() throws IOException {
+		count = init(br.readLine());
+		dp = new Long[count + 1];
+	}
+    
+    public static void main(String args[]) throws IOException{
+        make();
+        solution();
     }
 }
