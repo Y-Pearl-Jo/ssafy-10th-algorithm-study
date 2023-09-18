93d6002894e0ba4c0973587bc4f7ffcbf8a8c168
조담현 <j38903639@gmail.com>
2023-09-18 20:18:37 +0900

    Create Main.java

---
+++ DP/기초1890. 점프/조담현/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[n][n];
+
+		int[][] d = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		d[0][0] = 1;
+    
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (d[i][j] == 0)
+					continue;
+				if (i == n - 1 && j == n - 1)
+					continue;
+				int move = arr[i][j];
+				int nr = i + move;
+				int nc = j + move;
+				if (nr < n) {
+					d[nr][j]++;
+				}
+
+				if (nc < n) {
+					d[i][nc]++;
+				}
+			}
+		}
+
+		System.out.println(d[n - 1][n - 1]);
+	}
+
+}
