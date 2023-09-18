93662f3b953957895eef1e27833171c128fff63a
조담현 <j38903639@gmail.com>
2023-09-18 21:26:11 +0900

    Update Main.java

--- DP/기초1890. 점프/조담현/Main.java
+++ DP/기초1890. 점프/조담현/Main.java
@@ -10,9 +10,9 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		long[][] arr = new int[n][n];
+		int[][] arr = new int[n][n];
 
-		long[][] d = new int[n][n];
+		long[][] d = new long [n][n];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
