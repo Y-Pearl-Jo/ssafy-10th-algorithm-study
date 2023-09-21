9da4724f4dd252bbb8108283fa803a48fb67cab7
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-09-21 15:52:30 +0900

    Update Main.java

--- DP/심화12865. 평범한 배낭/이재신/Main.java
+++ DP/심화12865. 평범한 배낭/이재신/Main.java
@@ -3,16 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-class Thing {
-	int weight;
-	int value;
 
-	public Thing(int weight, int value) {
-		this.weight = weight;
-		this.value = value;
-	}
-}
-
+// 메모리 : 51536 KB 시간 : 172 ms
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
@@ -24,11 +16,11 @@ public class Main {
 		for (int curw = 1; curw < maxWeight + 1; curw++) {
 			for (int w = 1; w < N + 1; w++) {
 				if (bag[w].weight > curw)
-          //현재 단계에서 넣을 수 없는 무게
+          			//현재 단계에서 넣을 수 없는 무게
 					dp[w][curw] = dp[w - 1][curw];
 				else
-          // 가방에 넣을지 말지 value max 값을 비교하여 대입
-          // 허용 최대 무게를 넘지 않기 위해 curw - bag[w].weight
+          			// 가방에 넣을지 말지 value max 값을 비교하여 대입
+          			// 허용 최대 무게를 넘지 않기 위해 curw - bag[w].weight
 					dp[w][curw] = Math.max(dp[w-1][curw - bag[w].weight] + bag[w].value, dp[w-1][curw]);
 			}
 		}
@@ -57,4 +49,14 @@ public class Main {
 	static int init(String str) {
 		return Integer.parseInt(str);
 	}
+
+	static class Thing {
+	int weight;
+	int value;
+
+	public Thing(int weight, int value) {
+		this.weight = weight;
+		this.value = value;
+	}
+}
 }
