579769d8e4dfc9f9328db45d9c9fb05b7696e64f
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2023-09-25 17:52:37 +0900

    Update Main.java

--- Tree DP/심화2533. 사회망 서비스(SNS)/권준구/Main.java
+++ Tree DP/심화2533. 사회망 서비스(SNS)/권준구/Main.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class p2533 {
+public class Main {
     static int n;
-    static boolean[] visited;
+    static boolean[] visit;
     static List<Integer>[] graph;
     static int[][] dp;  // [y][x]일 때, y : 노드 번호, x : 0 -> 해당 노드번호가 earlyAdaptor가 아닐때, 1 -> 해당 노드번호가 earlyAdaptor일 때
 
@@ -15,7 +15,7 @@ public class p2533 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.valueOf(br.readLine());
         dp = new int[n + 1][2];  // 각 노드가 얼리어답터인지 아닌지에 따른 dp 배열
-        visited = new boolean[n + 1];
+        visit = new boolean[n + 1];
         graph = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
@@ -35,12 +35,12 @@ public class p2533 {
     }
 
     static void dfs(int number) {
-        visited[number] = true;
+    	visit[number] = true;
         dp[number][0] = 0;  // 해당 number가 얼리어답터가 아닌 경우
         dp[number][1] = 1;  // 해당 number가 얼리어답터인 경우(우선 시작 시 해당 지점 얼리어답터이므로 개수 1)
 
         for (int child : graph[number]) {
-            if (!visited[child]) {  // dfs 중복 방문 방지
+            if (!visit[child]) {  // dfs 중복 방문 방지
                 dfs(child);  // dfs 재귀호출을 통해 자식 노드의 dp값을 미리 구한다.
                 dp[number][0] += dp[child][1];  // 자식 노드가 무조건 얼리어답터여야한다.
                 dp[number][1] += Math.min(dp[child][0], dp[child][1]);  // 왜냐하면 최소의 얼리어답터 인원을 뽑기 때문에 자식 노드가 얼리어답터 일수도, 아닐수도 있다.
