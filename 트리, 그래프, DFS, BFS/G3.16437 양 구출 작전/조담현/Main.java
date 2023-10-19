9e443588b01cd9e0d28c6b197c8aee5725251e4e
조담현 <j38903639@gmail.com>
2023-10-19 20:13:01 +0900

    Update Main.java

--- 트리, 그래프, DFS, BFS/G3.16437 양 구출 작전/조담현/Main.java
+++ 트리, 그래프, DFS, BFS/G3.16437 양 구출 작전/조담현/Main.java
@@ -22,7 +22,9 @@ public class Main {
 		for (int i = 1; i <= N; i++) {
 			islands[i] = new Island();
 		}
+		
 		islands[1].animal = "S";
+		
 		for (int i = 2; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String animal = st.nextToken();
@@ -30,10 +32,12 @@ public class Main {
 			int destination = Integer.parseInt(st.nextToken());
 			islands[i].dest = destination;
 			islands[destination].chlidren.add(i);
+			// 양인지 늑대인지 동물 타입을 계산해주고, count를 저장해준다.
 			if (animal.equals("S")) {
 				islands[i].animal = "S";
 				islands[i].count = count;
-			} else {
+			} 
+			else {
 				islands[i].animal = "W";
 				islands[i].count = count;
 			}
@@ -44,12 +48,13 @@ public class Main {
 
 	public static long moveSheep(Island now) {
 		long sum = 0;
-		
+
+		// 자식노드들의 합을 먼저 모두 계산한다.
 		for(int next : now.chlidren) {
 			sum += moveSheep(islands[next]);
 		}
 		
-		
+		// 자식노드들의 합을 모두 계산한 후, 내 노드가 양인지 늑대인지에 따라 합을 구해주고 부모 노드에게 return한다.
  		if(now.animal.equals("S")) {
 			return sum + now.count;
 		} else {
