2eb2ebb27c55a12b7496b039c28f655066a76961
조담현 <j38903639@gmail.com>
2024-01-14 21:38:13 +0900

    Update Main.java

--- 문자열(KMP, 트라이)/G3.1701 Cubeditor/조담현/Main.java
+++ 문자열(KMP, 트라이)/G3.1701 Cubeditor/조담현/Main.java
@@ -9,14 +9,14 @@ public class Main {
 		String s = br.readLine();
 		int max = 0;
         
-        // 모든 부분 문자열에 대해 탐색
+        	// 모든 부분 문자열에 대해 탐색
 		for(int i = 0; i < s.length(); i++) {
 			max = Math.max(max, getpi(s.substring(i, s.length())));
 		}
 		System.out.print(max);
 	}
 	
-    // 해당 문자열 내에 존재하는 부분 문자열 중 접두사와 접미사가 같은 문자열의 최대 길이 구하기
+    	// 해당 문자열 내에 존재하는 부분 문자열 중 접두사와 접미사가 같은 문자열의 최대 길이 구하기
 	public static int getpi(String s) {
 		int[] pi = new int[s.length()];
 		int j = 0; // 접두사 비교 인덱스
@@ -26,7 +26,7 @@ public class Main {
 				j = pi[j - 1];
 			}
 			if(s.charAt(i) == s.charAt(j)) {
-                pi[i] = ++j;
+                		pi[i] = ++j;
 				max = Math.max(max, pi[i]);
 			}
 		}
